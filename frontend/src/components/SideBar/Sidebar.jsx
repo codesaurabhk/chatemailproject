c6dc52a4f220adac72d8828b45ab8523c7d4bc7a
Avinash-netario <135868337+codesaurabhk@users.noreply.github.com>
2025-07-03 16:44:07 +0530

    Update Sidebar.jsx

--- frontend/src/components/SideBar/Sidebar.jsx
+++ frontend/src/components/SideBar/Sidebar.jsx
@@ -13,7 +13,7 @@ import { FaSquarePlus } from "react-icons/fa6";
 import { useState } from 'react';
 import { FaFolder } from "react-icons/fa";
 
-import { useState } from 'react';
+
 import EmailModal from '../EmailModal/EmailModal';
 
 
