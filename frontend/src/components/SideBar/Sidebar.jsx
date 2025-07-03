10a9ab4dcbe516c27ae79e0533386c6618cee7f4
Avinash-netario <135868337+codesaurabhk@users.noreply.github.com>
2025-07-03 10:17:36 +0530

    add

--- frontend/src/components/SideBar/Sidebar.jsx
+++ frontend/src/components/SideBar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Sidebar.css'
 import Img from '../../assets/images/dp.jpg'
+import { FaRegEdit } from "react-icons/fa";
+import { HiOutlineInbox } from "react-icons/hi";
 
 const Sidebar = () => {
   return (
@@ -8,14 +10,15 @@ const Sidebar = () => {
         <div className='first'>
             <div ><div ><img className='dp' src={Img} alt="" /></div></div>
             <div><div className='name'>James Hong</div>
-            <div className='email'>Jnh343@example.com</div></div>
+            <div className='emailhong'>Jnh343@example.com</div></div>
         </div>
 
         <div>
-            <div className='compose'>Compose</div>
+            <div className='compose'><FaRegEdit />Compose</div>
         </div>
-        <div>
+        <div className='email'>
             <div>Emails </div>
+            <div> <HiOutlineInbox />Inbox</div>
             <div>Starred</div>
             <div>Sent</div>
             <div>Drafts</div>
