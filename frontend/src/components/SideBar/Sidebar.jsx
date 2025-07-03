1540a3b766560dd0b9c2b44c13838d8dca304a76
Avinash-netario <135868337+codesaurabhk@users.noreply.github.com>
2025-07-03 15:59:25 +0530

    sidebar done

--- frontend/src/components/SideBar/Sidebar.jsx
+++ frontend/src/components/SideBar/Sidebar.jsx
@@ -8,10 +8,17 @@ import { FaRegFilePdf } from "react-icons/fa";
 import { RiDeleteBinLine, RiSpam2Line } from "react-icons/ri";
 import { ImCompass } from "react-icons/im";
 import { PiUploadLight } from "react-icons/pi";
-import { IoSquareOutline } from "react-icons/io5";
+import { FaAngleDown,FaAngleUp } from "react-icons/fa6";
+import { FaSquarePlus } from "react-icons/fa6";
+import { useState } from 'react';
+import { FaFolder } from "react-icons/fa";
+
 
 
 const Sidebar = () => {
+    const [showMore, setshowMore] = useState(false);
+    const [showMores, setshowMores] = useState(false);
+    const [showMoref, setshowMoref] = useState(false);
   return (
     <div className='sidebarm'>
       <div className='user-box'>
@@ -32,26 +39,38 @@ const Sidebar = () => {
         <div className='item'><span><FaRegFilePdf />Drafts</span> <span >12</span></div>
         <div className='item'><span><RiDeleteBinLine />Deleted </span><span >08</span></div>
         <div className='item'><span><RiSpam2Line />Spam </span><span >0</span></div>
+        {showMore && (<>
         <div className='item'><span><ImCompass />Important </span><span >12</span></div>
         <div className='item'><span><PiUploadLight />All Emails </span><span >34</span></div>
-        <div className='item'>Less </div>
-        {/* <div className='item'><ImCompass />Show More</div> */}
+        </>)}
+        
+        <div className='item' onClick={() => setshowMore(prev => !prev)}>{showMore ? "Show Less" : "Show More"} {!showMore && <FaAngleDown />} {showMore && <FaAngleUp /> } </div>
       </div>
 
       <div className='section'>
-        <div className='section-title'>Labels</div>
-        <div className='label'><span className='dot team'></span> Team Events</div>
-        <div className='label'><span className='dot work'></span> Work</div>
-        <div className='label'><span className='dot external'></span> External</div>
-        <div className='label'><span className='dot projects'></span> Projects</div>
-        <div className='item'>Show More</div>
+        <div className='section-title'>Labels <div className='plus'><FaSquarePlus /></div></div>
+        <div className='label'> <input type="checkbox"   className='dot team' /> Team Events</div>
+        <div className='label'> <input type="checkbox" className='dot work' /> Work</div>
+        <div className='label'> <input type="checkbox" className='dot external' /> External</div>
+        <div className='label'> <input type="checkbox" className='dot projects'/> Projects</div>
+        {showMores && (<> 
+        <div className='label'> <input type="checkbox" className='dot applications'/> Applications</div>
+        <div className='label'> <input type="checkbox" className='dot design'/> Design</div>
+        </>)}
+        <div className='item' onClick={()=> setshowMores(prev => !prev)}>{showMores ? "Show Less" : "Show More"}{!showMores && <FaAngleDown/>} {showMores && <FaAngleUp/>}</div>
       </div>
 
       <div className='section'>
-        <div className='section-title'>Folders</div>
-        <div className='label'><span className='dot projects'></span> Projects</div>
-        <div className='label'><span className='dot personal'></span> Personal</div>
-        <div className='label'><span className='dot finance'></span> Finance</div>
+        <div className='section-title'>Folders <div className='plus'><FaSquarePlus /></div></div>
+        <div className='label'><FaFolder className='projects'/> Projects</div>
+        <div className='label'><FaFolder className='personal'/> Personal</div>
+        <div className='label'><FaFolder className='finance'/> Finance </div>
+        {showMoref && (<>
+         <div className='label'><FaFolder className='projectk'/>Projects </div>
+        </>)}
+        <div className='item' onClick={()=> setshowMoref(prev => !prev)}>{showMoref ? "Show Less" : "Show More"} {!showMoref && <FaAngleDown/>} </div>
+       
+        
       </div>
     </div>
   );
