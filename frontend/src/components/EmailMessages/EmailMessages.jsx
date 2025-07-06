3d7b683790a54e9a3082cb8d2665a8b164ca6af2
Akashkumar7488 <138307042+Akashkumar7488@users.noreply.github.com>
2025-07-06 11:34:14 +0530

    remove commented code from EmailMessage.jsx

--- frontend/src/components/EmailMessages/EmailMessages.jsx
+++ frontend/src/components/EmailMessages/EmailMessages.jsx
@@ -1,205 +1,3 @@
-// import React, { useState } from "react";
-// import "../EmailMessages/EmailMessages.css";
-// import { IoIosSearch, IoMdSettings } from "react-icons/io";
-// import { RiFilterOffLine } from "react-icons/ri";
-// import { BiRefresh } from "react-icons/bi";
-// import { AiOutlineSetting } from "react-icons/ai";
-// import { BsDot } from "react-icons/bs";
-// import { HiOutlineDotsHorizontal } from "react-icons/hi";
-// import { AiOutlineFolderOpen } from "react-icons/ai";
-// import { GrGallery } from "react-icons/gr";
-// import { AiFillStar } from "react-icons/ai";
-// import EmailData from "../EmailMessages/emailData.json"
-// import { useEffect } from "react";
-// import axios from "axios";
-
-// const EmailMessages = () => {
-//     const [search, setSearch] = useState("");
-//     const [emails, setEmails] = useState([])
-
-//     useEffect(() => {
-//       const fetchEmail = async () => {
-//         try {
-//           const res = await axios.get("http://localhost:5000/api/email/receive")
-//           setEmails(res.data.data)
-//         }catch(error) {
-//           console.error("Failed to fetch emails", error)
-//         }
-//       }
-//       fetchEmail();
-//     }, []);
-
-//   return (
-//     <div className="mainemailmessage">
-//       <div className="header">
-//         {/* inbox */}
-//         <div className="inbox">
-//           <span style={{ color: "black", fontSize: "18px", fontWeight: 600 }}>
-//             Inbox
-//           </span>
-//           <span className="twothreemail">
-//             2345 Emails{" "}
-//             <span
-//               style={{
-//                 fontSize: "22px",
-//                 borderRadius: "50%",
-//                 fontWeight: "bold",
-//               }}
-//             >
-//               <BsDot style={{ color: "#fba64b", marginTop: "10px" }} />
-//             </span>{" "}
-//             56 Unread
-//           </span>
-//         </div>
-//         {/* filter */}
-//         <div className="filter">
-//           <span className="searchinputdiv">
-//             <span style={{ marginTop: "5px" }}>
-//               <IoIosSearch />
-//             </span>
-//             <input
-//               className="searchtext"
-//               type="text"
-//               placeholder="Search Email"
-//               value={search} onChange={(e) => setSearch(e.target.value)}
-//             />
-//           </span>
-//           <span className="settingrefreshdiv">
-//             <RiFilterOffLine />
-//             <AiOutlineSetting />
-//             <BiRefresh onClick={() => window.location.reload()} />
-//           </span>
-//         </div>
-//       </div>
-//       {/* email message div */}
-//       <>
-//       {emails.filter((email) => 
-//       email.name.toLowerCase().includes(search.toLowerCase()) || 
-//       email.subject.toLowerCase().includes(search.toLowerCase()) ||
-//       email.body.toLowerCase().includes(search.toLowerCase())
-//       ).map((email) => (
-//       <div className="justinmaindiv">
-//         <div className="justinleftrightmaindiv">
-//           {/* left */}
-//           <div className="justinmaindivleftdiv">
-//             <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-//               <input
-//                 type="checkbox"
-//                 style={{ width: "18px", height: "18px", borderRadius: "5px" }}
-//               />
-//               <span
-//                 style={{
-//                   backgroundColor: '',
-//                   color: "white",
-//                   borderRadius: "50%",
-//                   width:"40px", height:"40px", display:'flex', alignItems:'center', justifyContent:'center', objectFit:'contain'
-//                 }}
-//               >
-//                 {}
-//               </span>
-//             </div>
-//             <div style={{ display: "flex", flexDirection: "column" }}>
-//               <span
-//                 style={{
-//                   color: "",
-//                   fontSize: "18px",
-//                   fontWeight: 600,
-//                   marginBottom: "5px",
-//                 }}
-//               >
-//                 {email.name}
-//               </span>
-//               <span style={{ color: "#636363", fontSize: "16px" }}>
-//                 {email.subject}
-//               </span>
-//               <span style={{ color: "#636363", fontSize: "16px" }}>
-//                {email.body}
-//               </span>
-//             </div>
-//           </div>
-//           {/* right */}
-//           <div className="justinmaindivrightdiv">
-//             <span>
-//               <HiOutlineDotsHorizontal />
-//             </span>
-//             <span
-//               style={{
-//                 fontSize: "22px",
-//                 borderRadius: "50%",
-//                 fontWeight: "bold",
-//               }}
-//             >
-//               <BsDot style={{ color: 'red', fontSize: "30px" }} />
-//             </span>
-//             <span style={{ marginBottom: "5px" }}>{}</span>
-//           </div>
-//         </div>
-//         {/* folder gallery */}
-//         <div className="foldergallerydiv">
-//           <div
-//             style={{
-//               display: "flex",
-//               gap: "10px",
-//               color: "#676969",
-//               fontWeight: 600,
-//             }}
-//           >
-//             <span>
-//               <AiOutlineFolderOpen />
-//             </span>
-//             <span>3</span>
-//             <span>
-//               <GrGallery />
-//             </span>
-//             <span>{}</span>
-//           </div>
-//           <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-//             <span>
-//               <AiFillStar style={{ fontSize: "15px", color: "#fba64b" }} />
-//             </span>
-//             <span
-//               style={{
-//                 padding: "4px 7px",
-//                 backgroundColor: '',
-//                 borderRadius: "5px",
-//                 color: '',
-//               }}
-//             >
-//               <input type="checkbox" />
-//               <span>{email.name}</span>
-//             </span>
-//             <span
-//               style={{
-//                 padding: "4px 7px",
-//                 backgroundColor: "#010c27",
-//                 borderRadius: "45%",
-//                 color: "white",
-//                 fontSize: "12px",
-//               }}
-//             >
-//               +{}
-//             </span>
-//           </div>
-//         </div>
-//       </div>
-//       ))}
-//       </>   
-//     </div>
-//   );
-// };
-
-// export default EmailMessages;
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import "../EmailMessages/EmailMessages.css";
 import { IoIosSearch, IoMdSettings } from "react-icons/io";
@@ -223,7 +21,7 @@ const EmailMessages = () => {
   const [search, setSearch] = useState("");
   const [emails, setEmails] = useState([]);
 
-  const [selectedEmail, setSelectedEmail] = useState(null)
+  const [selectedEmails, setSelectedEmails] = useState(null)
 
 
   useEffect(() => {
@@ -238,7 +36,7 @@ const EmailMessages = () => {
             sender: {
               name,
               initials,
-              backgroundColor:  "#5e35b1"
+              backgroundColor: "#5e35b1"
             },
             subject: email.subject,
             messagePreview: email.body.slice(0, 50) + "...",  //trim preview
@@ -328,8 +126,8 @@ const EmailMessages = () => {
       </div>
       {/* email message div */}
       <>
-        {selectedEmail ? (
-          <EmailDetail email={selectedEmail} onBack={handleBackToInbox} />
+        {selectedEmails ? (
+          <EmailDetail email={selectedEmails} onBack={handleBackToInbox} />
         ) : (
           emails.filter((email) =>
             email.sender.name.toLowerCase().includes(search.toLowerCase()) ||
@@ -405,7 +203,7 @@ const EmailMessages = () => {
                   <span>
                     <AiOutlineFolderOpen />
                   </span>
-                  <span>{email.attachments.length}</span>
+                  <span>{email.attachments?.length}</span>
                   <span>
                     <GrGallery />
                   </span>
